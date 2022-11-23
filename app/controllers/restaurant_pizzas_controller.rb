58bc84f3398e935eb3c60f02836a845623ff1396
Vincent <makokhavomondi@gmail.com>
2022-11-23 22:50:30 +0300

    Complete the "POST /restaurant_pizzas" requirement

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,18 @@
 class RestaurantPizzasController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_when_creation_unsuccessful
+
+    def create
+        restaurant_pizza = RestaurantPizza.create!(restaurant_pizza_params)
+        pizza = restaurant_pizza.pizza
+        render json: pizza, serializer: RestaurantPizzaSerializer
+    end
+
+    private
+    def restaurant_pizza_params
+        params.permit(:price, :pizza_id, :restaurant_id)
+    end
+
+    def render_when_creation_unsuccessful(invalid)
+        render json: {errors: invalid.record.errors.full_messages}
+    end
 end
